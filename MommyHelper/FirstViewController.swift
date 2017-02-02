70a65ccce72252e3cbcea8c9fb3c7975cd2fc55a
Priyanka Joshi <khiladis@Priyankas-MacBook-Pro.local>
2017-02-01 21:37:29 -0800

    Initial Commit

---
+++ MommyHelper/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MommyHelper
+//
+//  Created by Priyanka Joshi on 2/1/17.
+//  Copyright © 2017 Priyanka Joshi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
